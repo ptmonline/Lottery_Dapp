02b1887c9bf82fa6cc5c919b480241cd8cab0838
ptmonline <ptmonlinework@gmail.com>
2019-02-06 14:17:40 +0100

    added test file

--- compile.js
+++ compile.js
@@ -2,8 +2,8 @@ const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
 
-const inboxPath = path.resolve(__dirname + '/contracts', 'Lottery.sol');
-const source = fs.readFileSync(inboxPath, 'utf8');
+const lotteryPath = path.resolve(__dirname + '/contracts', 'Lottery.sol');
+const source = fs.readFileSync(lotteryPath, 'utf8');
 
 // console.log(solc.compile(source, 1));
 // INFURA ENDPOINT = https://rinkeby.infura.io/v3/4404778bc8d64811b4f531e508d9817c
